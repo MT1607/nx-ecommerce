b4de85b2834b07315abd81f667da4f5268a3a0d4
MT1607 <nguyengiason5@gmail.com>
2025-11-24 08:31:03 +0700

    update: update script run and unit test gateway

--- apps/back-end/gateway-e2e/src/gateway/gateway.spec.ts
+++ apps/back-end/gateway-e2e/src/gateway/gateway.spec.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-describe('GET /api', () => {
-  it('should return a message', async () => {
-    const res = await axios.get(`/api`);
+describe('Get all users', () => {
+  it('Should return a array', async () => {
+    const res = await axios.get(`/api/users`);
 
     expect(res.status).toBe(200);
-    expect(res.data).toEqual({ message: 'Hello API' });
+    expect(res.data).toBeInstanceOf(Array);
   });
 });

abb023a7c9c331ccd22189bfb70809f18aeaec67
MT1607 <nguyengiason5@gmail.com>
2025-11-24 19:18:17 +0700

    feat: create db exception filter

---
+++ libs/src/db-exception/db-exception.ts
@@ -0,0 +1,47 @@
+// Trong thư mục microservice/src/common/filters/db-exception.filter.ts
+import {
+  Catch,
+  RpcExceptionFilter,
+  ArgumentsHost,
+  HttpStatus,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { QueryFailedError } from 'typeorm'; // Hoặc thư viện DB bạn dùng
+import { RpcException } from '@nestjs/microservices';
+
+@Catch(QueryFailedError) // Bắt các lỗi do DB ném ra
+export class DbExceptionFilter implements RpcExceptionFilter<QueryFailedError> {
+  catch(exception: QueryFailedError, host: ArgumentsHost): Observable<any> {
+    let statusCode: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+    let message: string = 'Database error occurred.';
+
+    const sqlState = (exception as any).code;
+
+    switch (sqlState) {
+      case '23505': // Unique Violation
+        statusCode = HttpStatus.CONFLICT; // 409
+        message = 'Data aldready exist.';
+        break;
+      case '23502': // Not Null Violation
+      case '22001': // String Truncation
+        statusCode = HttpStatus.BAD_REQUEST; // 400
+        message = 'Missing required user fields';
+        break;
+      case '23503': // Foreign Key Violation
+        statusCode = HttpStatus.CONFLICT; // 409
+        message = 'conflict with related data.';
+        break;
+      default:
+        break;
+    }
+
+    return throwError(
+      () =>
+        new RpcException({
+          statusCode,
+          message,
+          dbCode: sqlState,
+        })
+    );
+  }
+}
--- libs/src/index.ts
+++ libs/src/index.ts
@@ -1,3 +1,4 @@
 export * from './dto/create-user.dto.ts';
 export * from './entities/user.entity.ts';
 export * from './interface/user-interface.ts';
+export * from './db-exception/db-exception.ts';

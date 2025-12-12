b43c5abd1e2cf870c72342f755558f111cd2b08e
MT1607 <nguyengiason5@gmail.com>
2025-12-12 14:49:54 +0700

    feat: add security by arcjet for app

--- apps/ecommerce-be/src/app/app.module.ts
+++ apps/ecommerce-be/src/app/app.module.ts
@@ -1,13 +1,51 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import {
+  ArcjetModule,
+  detectBot,
+  fixedWindow,
+  shield,
+  validateEmail,
+  tokenBucket,
+  ArcjetGuard,
+} from '@arcjet/nest';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
+    ArcjetModule.forRoot({
+      isGlobal: true,
+      key: process.env.ARCJET_KEY!,
+      characteristics: ['ip.src'],
+      rules: [
+        shield({ mode: 'LIVE' }),
+        detectBot({ mode: 'LIVE', allow: ['CATEGORY:SEARCH_ENGINE'] }),
+        fixedWindow({
+          mode: 'LIVE',
+          window: '60s',
+          max: 2,
+        }),
+        validateEmail({
+          mode: 'LIVE',
+          deny: ['DISPOSABLE', 'INVALID', 'NO_MX_RECORDS'],
+        }),
+        tokenBucket({
+          mode: 'LIVE',
+          refillRate: 5, // Refill 5 tokens per interval
+          interval: 10, // Refill every 10 seconds
+          capacity: 10, // Bucket capacity of 10 tokens
+        }),
+      ],
+    }),
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: ArcjetGuard,
+    },
+  ],
 })
 export class AppModule {}

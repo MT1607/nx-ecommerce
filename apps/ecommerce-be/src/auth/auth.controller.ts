3bfbf5a425a56f784d5459e94e65d58f01ab1b16
MT1607 <nguyengiason5@gmail.com>
2025-12-20 07:43:40 +0700

    refactor: update using fastify

--- apps/ecommerce-be/src/auth/auth.controller.ts
+++ apps/ecommerce-be/src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ import {
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import type { Response, Request } from 'express';
+import type { FastifyReply, FastifyRequest } from 'fastify';
 
 @Controller('auth')
 export class AuthController {
@@ -24,16 +24,25 @@ export class AuthController {
   @HttpCode(HttpStatus.CREATED)
   async create(
     @Body() registerDto: RegisterDto,
-    @Res({ passthrough: true }) res: Response
+    @Res({ passthrough: true }) res: FastifyReply
   ) {
     const result = await this.authService.register(registerDto);
 
     if (result.tokens.refreshToken) {
-      res.cookie('refresh-token', result.tokens.refreshToken, {
+      res.setCookie('refresh-token', result.tokens.refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      });
+    }
+
+    if (result.tokens.accessToken) {
+      res.setCookie('access-token', result.tokens.accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'strict',
+        maxAge: 1 * 60 * 60 * 1000, // 1 hour
       });
     }
 
@@ -46,17 +55,26 @@ export class AuthController {
       });
     }
 
-    return {
-      message: 'Create account success, please verify your email!',
-      accessToken: result.tokens.accessToken,
-    };
+    return res
+      .status(HttpStatus.CREATED)
+      .send({ message: 'User registered successfully' });
   }
 
   @Post('verify-email')
   @HttpCode(HttpStatus.OK)
-  verifyEmail(@Body('otp') otp: string, @Req() req: Request) {
+  async verifyEmail(
+    @Body('otp') otp: string,
+    @Req() req: FastifyRequest,
+    @Res() res: FastifyReply
+  ) {
     const token = req.cookies['email-verification'];
-    return this.authService.verifyEmail(token, otp);
+    if (!token) {
+      return res.status(HttpStatus.BAD_REQUEST).send({
+        message: 'Invalid or expired verification token',
+      });
+    }
+    const message = this.authService.verifyEmail(token, otp);
+    return res.status(HttpStatus.OK).send({ message });
   }
 
   @Get()
--- apps/ecommerce-be/src/main.ts
+++ apps/ecommerce-be/src/main.ts
@@ -7,14 +7,23 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { PostgresExceptionFilter } from './common/filters/postgres-exception.filter';
-import cookieParser from 'cookie-parser';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   const globalPrefix = 'api';
   app.useGlobalFilters(new PostgresExceptionFilter());
-  app.use(cookieParser());
   app.setGlobalPrefix(globalPrefix);
+  app.register(fastifyCookie, {
+    secret: process.env.COOKIE_SECRET,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
